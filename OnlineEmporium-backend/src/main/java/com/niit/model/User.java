3f8653022299cda63397ef84ff0f5c7e7a65a678
SUSMITHA <SUSMITHA@DESKTOP-AI7SRJH>
2018-04-18 10:41:54 +0530

    This is my project upto 17-04-2018

--- OnlineEmporium-backend/src/main/java/com/niit/model/User.java
+++ OnlineEmporium-backend/src/main/java/com/niit/model/User.java
@@ -4,7 +4,6 @@ import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
-import com.niit.model.Customer;
 @Entity
 public class User {
 	@Id
